ac28a09eb3a7a0e30d5de13ae76b7319fbf6b4df
simon <scriado.f@gmail.com>
2021-12-07 15:43:04 +0100

    Created a Context and Provider, learnt how to access Context values and created a custom Context hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
 
 // components
 import SearchBar from './SearchBar'
@@ -7,8 +8,10 @@ import SearchBar from './SearchBar'
 import './Navbar.css'
 
 export default function Navbar() {
+    const { color } = useTheme()
+
     return (
-        <div className="navbar">
+        <div className="navbar" style={{ background: color }}>
             <nav>
                 <Link to="/" className="brand">
                     <h1>Cooking Ninja</h1>
